a427b3d63ef074dd039628541cbaa74c4faf7dcf
Michelle Kinda <resamichel@gmail.com>
2022-03-14 22:56:01 +0300

    100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,25 +1,33 @@
+#include <stdlib.h>
 #include <stdio.h>
+
 /**
- *main - function
- *Return: valve 0
- */
+* main - the function main use printf to print numbers
+*
+* Return: Always 0 (Sucess)
+*/
 int main(void)
 {
-int a;
-int b;
-int c;
-for (a = 0; a < 100; a++)
-{
-b = a / 10;
-c = a % 10;
-putchar(b + '0');
-putchar(c + '0');
-if (a < 99)
-{
-putchar(',');
-putchar(' ');
-}
-}
-putchar('\n');
-return (0);
+	int i;
+	int a = 0;
+	int j = 49;
+
+	for (i = 48; i <= 56; ++i)
+	{
+		for (; j <= 57; ++j)
+		{
+
+			putchar(i);
+			putchar(j);
+			if (i != 56 || j != 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+		a++;
+		j = 49 + a;
+	}
+	putchar('\n');
+	return (0);
 }
